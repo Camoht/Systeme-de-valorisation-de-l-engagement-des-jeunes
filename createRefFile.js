3f93aa3bdbae842c2e4f8cb4260fa2abc7e40a11
Camoht <104215342+Camoht@users.noreply.github.com>
2023-05-14 18:05:03 +0200

    Upload create PDF
    Peut faire des pdf
    Certains problèmes restent à résoudre :
    - comment faire un tableau dans un pdf (avec js)
    - comment ajouter des pages au pdf au fur et à mesure (avec js)
    - ajouter bouton pour retourner en arrière (avec html)

--- createRefFile.js
+++ createRefFile.js
@@ -1,4 +1,5 @@
 function nb_zero(i){
+    //Use to create a number with 3 figures.
     // i : (int).
     //Return a string ("00" or "0") depending on i's number of figures.
 
@@ -12,7 +13,7 @@ function nb_zero(i){
 }
 
 function create_HTML(){
-    //Create a new window with the chosen references
+    //Create a new window with the chosen references.
 
     let MyW=window.open("");
     let html="<head><title>Mes références</title>";
@@ -20,7 +21,8 @@ function create_HTML(){
     html+="<body><h1>Mes références</h1>";
 
     //Add the text of chosen references to html variable
-    for(let i=1; i<3; i++){
+    for(let i=1; i<=document.getElementById('valider').value; i++){
+        console.log(i);
         let ref_id="ref"+nb_zero(i)+i+".txt";
         if(document.getElementById(ref_id).checked==true){
             html+=document.getElementById("text_"+ref_id).innerHTML;
@@ -38,15 +40,39 @@ function create_HTML(){
 }
 
 function create_PDF(){
-    //
+    //Create a PDF file with the chosen references.
 
-    console.log("PDF");
+    //Setting the PDF file
+    var pdf = new jsPDF({
+        orientation: 'p',
+        unit: 'mm',
+        format: 'a5',
+        putOnlyUsedFonts:true
+    });
+    pdf.setFont("helvetica");
+    pdf.setFontSize(9);
+
+    //Add the text of chosen references to text variable
+    let text="";
+    for(let i=1; i<=document.getElementById('valider').value; i++){
+        let ref_id="ref"+nb_zero(i)+i+".txt";
+        if(document.getElementById(ref_id).checked==true){
+            text+=document.getElementById("text_"+ref_id).innerText;
+        }
+    }
+
+    //Fill the new PDF file
+    pdf.text(text, 20, 20);
+    pdf.save('Mes_references.pdf');
+
+    //Alert the user, that he created his PDF file
+    document.getElementById("alert").innerHTML="Vous avez créer une page HTML.</br>";
 }
 
 function create_file(){
-    //
+    //Create PDF file or an HTML one with the chosen references.
 
-    //Selected format
+    //Choose the selected format
     if(document.getElementById("HTML").checked==true){
         create_HTML();
     } else {
