69aa57fcd71ca22dac8bce936a3936659361e4fb
Zaviou <104215216+Zaviou@users.noreply.github.com>
2023-06-07 17:30:11 +0200

    add function test
    js still doesn't provide the variables.
    Missing functions : test mail adress, global function

--- createaccount.php
+++ createaccount.php
@@ -1,5 +1,5 @@
 <?php
-    if (isset($_POST)){
+/*    if (isset($_POST)){
 
         $name2=$_POST["name"]."panda";
         echo "name2 : ".$name2."<br/>";
@@ -12,6 +12,79 @@
     }
     if ($request == 2){
         echo "booto";
+    }*/
+
+    $_POST["name"]="char";
+    $_POST["surname"]="";
+    $_POST["birth"]="azertyuiop";
+
+    $_POST["email"]="azertyu@gmail.com";
+
+
+    function empty_field(){
+        if ($_POST["name"]==""){
+            return 1;
+        }
+        else if ($_POST["surname"]==""){
+            return 1;
+        }
+        else if ($_POST["birth"]==""){
+            return 1;
+        }
+        else if ($_POST["email"]==""){
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
+
+    function format_date(){
+        echo strlen($_POST["birth"]);
+        if(strlen($_POST["birth"])!=10){
+            return 1;
+        }
+        else{
+            list($day, $month, $year) = explode('/', $_POST["birth"]);
+
+            if (checkdate($month, $day, $year)==0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+    }
+
+
+    function reused_mail (){
+        $folder_names=scandir('Data/');
+        $folder_names=array_diff($folder_names,[".",".."]);
+        
+
+        foreach ($folder_names as $user_id){
+            $file=fopen("Data/".$user_id."/user.txt", 'r');
+            for($i=0; $i<3; $i++){
+                fgets($file);
+            }
+            $mail=fgets($file);
+            if($mail==$_POST["email"]){
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+
+
+
+    if (reused_mail()==0){
+        echo"bananana";
+    }
+    else{
+        echo"nooOOOoooOOOO";
+    }
+
+
 
 ?>
