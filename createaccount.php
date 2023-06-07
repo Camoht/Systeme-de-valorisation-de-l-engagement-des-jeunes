6d9c0470c9b99c55c9f8d6886dae1364a4b0bb86
Zaviou <104215216+Zaviou@users.noreply.github.com>
2023-06-07 14:28:54 +0200

    update ajax

--- createaccount.php
+++ createaccount.php
@@ -1,5 +1,17 @@
 <?php
-    if (isset($_POST["name"])){
-        echo 'SIU';
+    if (isset($_POST)){
+
+        $name2=$_POST["name"]."panda";
+        echo "name2 : ".$name2."<br/>";
+
+        echo "name : ".$_POST["name"]."<br/>";
+        echo "surname : ".$_POST["surname"]."<br/>";
+        echo "birth : ".$_POST["birth"]."<br/>";
+        echo "email : ".$_POST["email"]."<br/>";
+        echo "couou";
     }
+    if ($request == 2){
+        echo "booto";
+    }
+
 ?>
