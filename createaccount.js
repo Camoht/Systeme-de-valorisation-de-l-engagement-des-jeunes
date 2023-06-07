e7ab24038ab90e419babaecac1c0659acb0e2c1c
Zaviou <104215216+Zaviou@users.noreply.github.com>
2023-06-07 14:28:54 +0200

    update ajax

--- createaccount.js
+++ createaccount.js
@@ -1,26 +1,47 @@
-function request_ajax(){
-    let xhttp = new XMLHttpRequest();
-    let name='';
-    let surnname='';
-    let birth='';
-    let email='';
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("choice").addEventListener("submit", function(event) {
 
-    xhttp.open("POST", "createaccount.php", true);
-    xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded")
+        // Prevent the unwanted transmission
+        event.preventDefault();
 
-    xhttp.onreadystatechange = function(){
+        //Initialise the variables
+        let name='';
+        let surname='';
+        let birth='';
+        let email='';
 
-        name=document.getElementById('name').value;
-        surname=document.getElementById('surname').value;
-        birth=document.getElementById('birth').value;
-        email=document.getElementById('email').value;
+        //Initialise the ajax request
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "createaccount.php?", true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-        if(this.status === 200 && this.readyState === XMLHttpRequest.DONE){
-            document.getElementById('phpreponse').innerHTML=xhttp.responseText;
-        }
-        xhttp.send("name="+encodedURIComponent(name)+"&surname="+encodedURIComponent(surname)+"&birth="+encodedURIComponent(birth)+"&email="+encodedURIComponent(email));
+        //Trigger the following instructions when the user clicks on the button
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
 
-    };
-    console.log('RATIO');
-}
-    
+                console.log("Je rempli les valeurs");
+
+                //Get written information by the user
+                name=document.getElementById('name').value;
+                surname=document.getElementById('surname').value;
+                birth=document.getElementById('birth').value;
+                email=document.getElementById('email').value;
+
+                //Display php response
+                document.getElementById("phpreponse").innerHTML = xhr.responseText;
+
+                xhr.send("name="+encodeURIComponent(name)/*+"&surname="+encodeURIComponent(surname)+"&birth="+encodeURIComponent(birth)+"&email="+encodeURIComponent(email)*/);
+            }
+        };
+
+        console.log (name)
+        console.log (surname)
+        console.log (birth)
+        console.log (email)
+
+        //Send variables to php
+        //xhr.send("name="+encodeURIComponent(name)/*+"&surname="+encodeURIComponent(surname)+"&birth="+encodeURIComponent(birth)+"&email="+encodeURIComponent(email)*/);
+
+        console.log("Fin du js");
+    });
+});
