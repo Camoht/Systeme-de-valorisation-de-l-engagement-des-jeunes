dcd007da0a6ddbdfa2b7fc37bfe55ca3f1bc565d
Jagadai <106830744+Jagadai@users.noreply.github.com>
2023-06-11 00:09:50 +0200

    Add files via upload

---
+++ Remerciement_referent.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Remerciement</title>
+        <meta charset="utf-8">
+        <?php
+         //Include some files
+            include 'constants.php';
+            echo '<link rel="stylesheet" type="text/css" href='.$GLOBALS["File"]["css"]["Referent"].'>';
+        ?>
+    </head>
+    <body>
+        <?php
+            echo $BANDEAUREFERENT;
+        ?>
+        <table>
+            <tr>
+                <h4>Vos modifications ont été prises en compte, merci.</h4>
+            </tr>
+        </table>
+    </body>
+</html>
