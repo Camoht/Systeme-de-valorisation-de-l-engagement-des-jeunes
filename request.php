f0ffc96266c865738b8dba15c25630214bfe9617
Wanbl <106831185+Wanbl@users.noreply.github.com>
2023-06-11 13:46:04 +0200

    Adaptation du lien envoyé via request

--- request.php
+++ request.php
@@ -156,7 +156,7 @@
         fputs($newReferenceFile, $checkboxValues);
 
         //Create absolute link
-        $relativLink = "/ProjetWeb/referent.php?studentId=" . $_SESSION["User_id"] . "&refnb=" . $refNumberForm;
+        $relativLink = dirname($_SERVER['SCRIPT_NAME']) . "/referent.php?studentId=" . $_SESSION["User_id"] . "&refnb=" . $refNumberForm;
         $adresseSite = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
         $adresseSite .= $_SERVER['HTTP_HOST'];
         $completeadresse = $adresseSite . $relativLink;
