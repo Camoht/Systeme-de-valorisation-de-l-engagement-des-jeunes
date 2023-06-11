e4ed037b4cac18c1b5f21b4cfe3ab47220c2d3db
Jagadai <106830744+Jagadai@users.noreply.github.com>
2023-06-11 21:24:32 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,59 @@
+<html>
+  <head>
+    <?php
+    //Include some files
+    include 'constants.php';
+    echo '<link rel="stylesheet" type="text/css" href='.$GLOBALS["File"]["css"]["Partenaire"].'>';
+    ?>
+    <title>Acceuil</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+        <?php
+            echo $BANDEAUACCUEIL;
+        ?>
+        <table>
+            <tr id="sous-titre">
+                <td>De quoi s'agit-il?</td>              
+                <td>A qui s'adresse-t'il?</td>
+            </tr>
+            <tr>
+                <td>D'une opportunité : celle qu'un engagement quel qu'il soit puisse être considérer à sa juste valeur!</td>
+                <td>A vous, jeunes entre 16 et 30 ans, qui vous êtes investis spontanément dans une association ou dans tout type d'action formelle ou informelle, et qui avez partagé de votre temps, de votre temps, de votre énergie, pour apporter un soutien, une aide, une compétence.</td>
+                <td>A vous, employeurs, recruteurs en ressources humaines, représentants d'organismes de formation, qui recevez des jeunes, pour un emploi, un stage, un cursus de qualification, pour qui le savoir-être constitue le premier fondement de toute capacité humaine.</td>
+                
+            </tr>
+            <tr>
+            <td>Toute expérience est source d'enrichissement et se doit d'être reconnu largement.</td>
+            <td>A vous, responsable de structures ou référent d'un jour, qui avez bénéficié même ponctuellement de cette implication citoyenne!</td>
+                
+            <tr>
+                <td>Elle révèle un potentiel, l'expression d'un savoir-être à concrétiser.</td>
+                <td>C'est l'occasion de vous engager a votre tour pour ces jeunes en confirmant leur richesse pour en avoir été un temps les témoins mais aussi les bénéficiaires!</td>
+            </tr>
+        </table>
+        <h5 id="sous-titre2">Cet engagement est une ressource à valoriser au fil d'un parcours en 3 étapes:</h5>
+        <div class="categorie">
+                    <div class="titrecategorie1">
+                        <p>ETAPE 1</p>
+                        <p>la valorisation</p>
+                        <div class="explanation1">
+                            <p>Décrivez votre expérience et mettez en avant ce que vous en avez retiré.<p>
+                        </div>
+                    </div>
+                <div class="titrecategorie2">
+                    <p>ETAPE 2</p>
+                    <p>la confirmation</p>
+                    <div class="explanation2">
+                            <p>Décrivez votre expérience et mettez en avant ce que vous en avez retiré.<p>
+                        </div>
+                </div>
+                <div class="titrecategorie3">
+                    <p>ETAPE 3</p>
+                    <p>la consultation</p>
+                    <div class="explanation3">
+                            <p>Décrivez votre expérience et mettez en avant ce que vous en avez retiré.<p>
+                        </div>
+                </div>
+    </body>
+</html>
